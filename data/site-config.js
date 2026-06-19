db59ea5ebef89a9f8569f6f1053e4c742bd78ce8
chunheejae828-beep <chunheejae828@gmail.com>
2026-06-19 13:37:25 +0900

    AutoPilot: update data/site-config.js

---
+++ data/site-config.js
@@ -0,0 +1,7 @@
+window.siteConfig = {
+  "siteName": "Side Income Engine",
+  "primaryColor": "#059669",
+  "bgMain": "#fafaf9",
+  "fontHeading": "'Plus Jakarta Sans'",
+  "layoutStyle": "modern"
+};
